9707f51c889ca9f51c4e4fec4ed1d2db2c4e16f9
kevin flores <kevinfloresg1998@gmail.com>
2024-04-13 21:39:29 -0400

    dark light done

--- e/script.js
+++
@@ -1,7 +0,0 @@
-var toggle = document.getElementById('boton');
-var body = document.querySelector('body');
-
-toggle.onclick = function(){
-    toggle.classList.toggle('active');
-    body.classList.toggle('active');
-}
