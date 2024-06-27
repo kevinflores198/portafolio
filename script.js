8f84c359633f9ef7f8c5b820d192945d3dbff9e3
kevin flores <kevinfloresg1998@gmail.com>
2024-06-26 23:59:33 -0400

    better

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-var toggle = document.getElementById('boton');
-var body = document.querySelector('body');
+const toggle = document.getElementById('boton');
+const body = document.querySelector('body');
+const burger = document.querySelector('#buton-burger');
+const menu_options = document.querySelector('#menu-options');
+
+burger.addEventListener('click', function(){
+    menu_options.classList.toggle('aside-active');
+});
 
 toggle.onclick = function(){
     toggle.classList.toggle('active');
     body.classList.toggle('active');
 }
+
+window.addEventListener("scroll", () =>{
+    const scroll = document.documentElement.scrollTop;
+    if (scroll > 100) {
+        arrowUp.style.right = 20 + "px";
+    }else{
+        arrowUp.style.right = -100 + "px";
+    }
+})
